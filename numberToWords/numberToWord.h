9319c219c995e179cb4415635e9fb49bc890db34
alex89620109009 <alex89620109009@gmail.com>
2024-10-09 16:05:02 +0300

    Added constant arrays

--- numberToWords/numberToWord.h
+++ numberToWords/numberToWord.h
@@ -1,5 +1,9 @@
 #ifndef NUMBERTOWORD
 #define NUMBERTOWORD
-
+#include<string>
+namespace constants {
+	const std::string units[10]{  };
+	const std::string ranks[10]{  };
+}
 
 #endif // !NUMBERTOWORD
