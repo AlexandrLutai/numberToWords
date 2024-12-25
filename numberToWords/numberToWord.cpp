b4192518eb13af6a425c8d07339342a6dc52fd1a
alex89620109009 <alex89620109009@gmail.com>
2024-12-25 12:57:58 +0300

    Пересмотрена структура программы

--- numberToWords/numberToWord.cpp
+++ numberToWords/numberToWord.cpp
@@ -3,28 +3,48 @@
 
 
 
-void getDigitsInArray(unsigned long , unsigned short*, int );
+void getDigitsInArray(unsigned long, unsigned short*, int);
 void changeParams(unsigned long* number, unsigned short* numberOfDigit = nullptr);
-std::string getNumberName(unsigned short, unsigned short , unsigned short , unsigned short );
+std::string getNumberName(unsigned short, unsigned short, unsigned short, unsigned short);
 std::string getNumberNameWithValutes(unsigned short, unsigned short, unsigned short);
 
 
 
 
-//Выводит число словом для разряда тысяч.
-//Обробатывается два исключения, для 1 и 2
-std::string thousandExeption(short lastDigit,int numberDigit, int fullNumberDigit) {
-	if (fullNumberDigit == 3) {
-		switch (lastDigit)
-		{
-		case 1:
-			return "Одна";
-		case 2:
-			return "Две";
-		
-		}
+std::string getNumberAsWords(unsigned long number)
+{
+	if (!number) return "Ноль Рублей.";
+	unsigned short numberOfDigit{ 0 };
+	unsigned short digitArray[3];
+	//Обработка самой правой тройки чисел с валютой.
+	getDigitsInArray(number, digitArray, 3);
+	std::string numberName{ getNumberNameWithValutes(digitArray[0],digitArray[1],digitArray[2]) };
+	changeParams(&number);
+
+	while (number > 0)
+	{
+		//Обработка чисел, начиная с разряда тысяч
+		getDigitsInArray(number, digitArray, 3);
+		numberName = getNumberName(digitArray[0], digitArray[1], digitArray[2], numberOfDigit / 3) + numberName;
+		changeParams(&number, &numberOfDigit);
 	}
-	return constants::units[lastDigit][numberDigit];
+
+	return numberName;
+}
+
+
+void changeParams(unsigned long* number, unsigned short* numberOfDigit)
+{
+	if (numberOfDigit)*numberOfDigit += 3;
+	*number /= 1000;
+}
+void getDigitsInArray(unsigned long number, unsigned short* numberArray, int arrSize)
+{
+	for (int i = 0; i < arrSize; i++)
+	{
+		numberArray[i] = number % 10;
+		number /= 10;
+	}
 }
 
 //Получение названий чисел
@@ -35,7 +55,7 @@ std::string getHundredName(unsigned short hundred)
 std::string getTenName(unsigned short ten, bool tenExeption)
 {
 	std::string tenName{ "" };
-	if (!tenExeption) 
+	if (!tenExeption)
 	{
 		tenName = constants::tensNames[ten];
 	}
@@ -47,7 +67,7 @@ std::string getUnitName(unsigned short unit, bool tenExeption, short digitRank =
 	//В переменной isExeption хранится 0, если обрабатывается исключение десятков, в другом случае - 1
 	unsigned short isExeption{ !tenExeption };
 	//Обрабатывается исключение для тысяч, 0 - разряд тысяч, так как разряд с валютой обрабатывается отдельно.
-	if (digitRank == 0) 
+	if (digitRank == 0)
 	{
 		//Если обрабатывается исключение для десятков, то в переменной isExeption хранится 0, 
 		//результатом данного выражения будет 0, что является индексом вложенного массива с исключением для десятков
@@ -60,22 +80,22 @@ std::string getUnitName(unsigned short unit, bool tenExeption, short digitRank =
 	}
 	return constants::unitsNames[isExeption][unit];
 }
-std::string getRankName(unsigned short unit, bool tenExeption, unsigned short digitRank) 
+std::string getRankName(unsigned short unit, bool tenExeption, unsigned short digitRank)
 {
 	return constants::ranksNames[digitRank][!tenExeption * unit];
 }
 
 
 
-std::string getNumberName(unsigned short unit,  unsigned short ten, unsigned short hundred, unsigned short digitRank)
+std::string getNumberName(unsigned short unit, unsigned short ten, unsigned short hundred, unsigned short digitRank)
 {
 
 	int summAllDigits{ hundred + ten + unit };
-	
+
 	if (summAllDigits)
 	{
 		bool tensExeption{ ten == 1 };
-		return getHundredName(hundred) + getTenName(ten, tensExeption) 
+		return getHundredName(hundred) + getTenName(ten, tensExeption)
 			+ getUnitName(unit, tensExeption, digitRank) + getRankName(unit, tensExeption, digitRank);
 	}
 	return "";
@@ -93,13 +113,12 @@ std::string getNumberNameWithValutes(unsigned short unit, unsigned short ten, un
 {
 	int summAllDigits{ hundred + ten + unit };
 
-	
-		bool tensExeption{ ten == 1 };
-		return getHundredName(hundred) + getTenName(ten, tensExeption)
-			+ getUnitName(unit, tensExeption) + getValuteName(unit, tensExeption);
-	
-}
 
+	bool tensExeption{ ten == 1 };
+	return getHundredName(hundred) + getTenName(ten, tensExeption)
+		+ getUnitName(unit, tensExeption) + getValuteName(unit, tensExeption);
+
+}
 
 
 
--- numberToWords/numberToWords.cpp
+++ numberToWords/numberToWords.cpp
@@ -8,7 +8,7 @@
 int main()
 {
     setlocale(LC_ALL, "Russian");
-
+    ntwTests::runTests();
        
     
     
